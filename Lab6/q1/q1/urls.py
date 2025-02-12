e6daaebead55673fb49be6c4d2702547690e869c
Disha Jain <dishajain1802@gmail.com>
2025-02-12 09:53:33 +0530

    Update urls.py

--- Lab6/q1/q1/urls.py
+++ Lab6/q1/q1/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path , include
 
 urlpatterns = [
-    path ('admin/', admin.site.urls),
-    re_path ('', include('calcapp.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('calcapp.urls')),
 ]
