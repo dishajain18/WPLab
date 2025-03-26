4dee0a1840dfd5f1b4dfbccd7ee87a51870bbc60
Disha Jain <dishajain1802@gmail.com>
2025-03-26 15:35:07 +0530

    Delete q3 directory

--- q3/humanapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Human
-
-admin.site.register(Human)
--- q3/humanapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Human
-
-class HumanForm(forms.ModelForm):
-    class Meta:
-        model = Human
-        fields = '__all__'
